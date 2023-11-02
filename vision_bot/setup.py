5d1a35ba6f06e9314b5f55e39de8383de54dc50c
kush0072001 <kaushal.bhargava200103@gmail.com>
2023-11-02 18:43:58 +0530

    section 1 complete

---
+++ vision_bot/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'vision_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kaushal',
+    maintainer_email='kaushal@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pub = vision_bot.pub:main',
+            'sub = vision_bot.sub:main',
+        ],
+    },
+)
